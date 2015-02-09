01cb522cffea72dcea68f8494efd29226c29e676
davamix <davamix@gmail.com>
2015-02-09 19:21:13 +0100

    Comments in code

--- UltrasonicSensorHC-SR04/UltrasonicSensorHC-SR04/Program.cs
+++ UltrasonicSensorHC-SR04/UltrasonicSensorHC-SR04/Program.cs
@@ -23,38 +23,55 @@ namespace UltrasonicSensorHC_SR04
 			_triggerPort = new OutputPort(Pins.GPIO_PIN_D5, false);
 
 			_echoPort.OnInterrupt += EchoPortOnOnInterrupt;
-
-			_minTicks = 6200L;
-
+			
 			while (true)
-			{
-				_triggerPort.Write(true);
-				Thread.Sleep(1);
-
-				_endTick = 0L;
-				_beginTick = DateTime.Now.Ticks;
-
-				_triggerPort.Write(false);
-				Thread.Sleep(50);
-
-				if (_endTick > 0L)
-				{
-					var elapsed = _endTick - _beginTick;
-
-					elapsed -= _minTicks;
-
-					Debug.Print(elapsed.ToString());
-				}
-				else
-				{
-					Debug.Print("-1");
-				}
-			}
+				Run();
 		}
 
+		private static void Run()
+		{
+			_endTick = 0L; // Reset the end time.
+			_beginTick = DateTime.Now.Ticks; // Time when the signal is thrown.
+
+			_triggerPort.Write(true); // Start to emit signal
+			
+			Thread.Sleep(2); // Time emiting the signal
+			
+			_triggerPort.Write(false); // Stop to emit signal
+
+			Thread.Sleep(50); // Sleep to give time to the EchoPortOnOnInterrupt event to throw it.
+
+			if (_endTick > 0L)
+			{
+				var elapsed = _endTick - _beginTick; // Calculate the round trip time from trigger to echo port after bouncing
+
+				Debug.Print(CalculateDistance(elapsed).ToString());
+			}
+			else
+			{
+				Debug.Print("-1");
+			}
+
+			Thread.Sleep(250);
+		}
+
+		// Echo received the signal after bouncing
 		private static void EchoPortOnOnInterrupt(uint data1, uint data2, DateTime time)
 		{
 			_endTick = time.Ticks;
+		}
+
+		/// <summary>
+		/// Calculate distance from delay time.
+		/// </summary>
+		/// <param name="time">Time in Ticks since the signal is thrown from the Trigger to received into Echo</param>
+		/// <returns></returns>
+		private static float CalculateDistance(long time)
+		{
+			// 343 m/s -> 34.3 cm/ms --> http://hyperphysics.phy-astr.gsu.edu/hbase/sound/souspe.html
+			// 2 -> time round trip
+
+			return (float)(34.3*TimeSpan.FromTicks(time).Milliseconds)/2;
 		}
 	}
 }
