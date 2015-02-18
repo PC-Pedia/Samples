26631a79d0e1a097d32157a65687adf06ddaff99
davamix <davamix@gmail.com>
2015-02-18 23:43:11 +0100

    Run method rewrited

--- UltrasonicSensorHC-SR04/UltrasonicSensorHC-SR04/Program.cs
+++ UltrasonicSensorHC-SR04/UltrasonicSensorHC-SR04/Program.cs
@@ -23,42 +23,58 @@ namespace UltrasonicSensorHC_SR04
 			_triggerPort = new OutputPort(Pins.GPIO_PIN_D5, false);
 
 			_echoPort.OnInterrupt += EchoPortOnOnInterrupt;
-			
+			_triggerPort.Write(true); // Start to emit signal
 			while (true)
 				Run();
 		}
 
 		private static void Run()
 		{
-			_endTick = 0L; // Reset the end time.
-			_beginTick = DateTime.Now.Ticks; // Time when the signal is thrown.
+			//Thread.Sleep(250);
+			//_endTick = 0L; // Reset the end time.
+			//_beginTick = DateTime.Now.Ticks; // Time when the signal is thrown.
 
-			_triggerPort.Write(true); // Start to emit signal
+			//_triggerPort.Write(true); // Start to emit signal
 			
-			Thread.Sleep(2); // Time emiting the signal
+			//Thread.Sleep(2); // Time emiting the signal
 			
-			_triggerPort.Write(false); // Stop to emit signal
+			//_triggerPort.Write(false); // Stop to emit signal
 
-			Thread.Sleep(50); // Sleep to give time to the EchoPortOnOnInterrupt event to throw it.
+			//Thread.Sleep(50); // Sleep to give time to the EchoPortOnOnInterrupt event to throw it.
 
+			//if (_endTick > 0L)
+			//{
+			//	var elapsed = _endTick - _beginTick; // Calculate the round trip time from trigger to echo port after bouncing
+
+			//	Debug.Print(CalculateDistance(elapsed).ToString());
+			//}
+			//else
+			//{
+			//	Debug.Print("-1");
+			//}
+
+			_endTick = 0L;
+			_beginTick = DateTime.Now.Ticks;
+			_triggerPort.Write(true);
+			Thread.Sleep(500);
+			_triggerPort.Write(false);
+		}
+
+		// Echo received the signal after bouncing
+		private static void EchoPortOnOnInterrupt(uint data1, uint data2, DateTime time)
+		{
+			
+			_endTick = time.Ticks;
+			
 			if (_endTick > 0L)
 			{
 				var elapsed = _endTick - _beginTick; // Calculate the round trip time from trigger to echo port after bouncing
 
 				Debug.Print(CalculateDistance(elapsed).ToString());
 			}
-			else
-			{
-				Debug.Print("-1");
-			}
 
-			Thread.Sleep(250);
-		}
-
-		// Echo received the signal after bouncing
-		private static void EchoPortOnOnInterrupt(uint data1, uint data2, DateTime time)
-		{
-			_endTick = time.Ticks;
+			
+			//Debug.Print("Ticks: " + TimeSpan.FromTicks(time.Ticks).Milliseconds);
 		}
 
 		/// <summary>
@@ -71,6 +87,7 @@ namespace UltrasonicSensorHC_SR04
 			// 343 m/s -> 34.3 cm/ms --> http://hyperphysics.phy-astr.gsu.edu/hbase/sound/souspe.html
 			// 2 -> time round trip
 
+			//I think it doesn't works with a time < 1ms 
 			return (float)(34.3*TimeSpan.FromTicks(time).Milliseconds)/2;
 		}
 	}
