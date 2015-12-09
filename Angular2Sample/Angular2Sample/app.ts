854acdbb890293e9ae413803274d17eb3a514f82
Daniel Valcarce <davamix@gmail.com>
2015-12-09 23:05:02 +0100

    Sample Angular 2 wit TS. Does not work :(

---
+++ Angular2Sample/Angular2Sample/app.ts
@@ -0,0 +1,24 @@
+﻿import {Component, View, bootstrap, formDirectives} from 'angular2/angular2';
+
+@Component({
+    selector: 'main-component'
+})
+
+@View({
+    directives: [formDirectives],
+    templateUrl: 'mainForm.html'
+})
+
+class MyComponent {
+    name: string;
+
+    hello(): void {
+        alert("hello, " + this.name);
+    }
+
+    constructor() {
+        this.name = "Me";
+    }
+}
+
+bootstrap(MyComponent);
